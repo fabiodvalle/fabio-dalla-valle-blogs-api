cf268cf0b501ef2aa807f692dce2abbf173849b3
Fabio Dalla Valle <fabiodvalle91@gmail.com>
2023-05-10 22:31:49 -0300

    requisito 7

---
+++ src/models/Category.js
@@ -0,0 +1,23 @@
+const Category = (sequelize, DataTypes) => {
+  const CategoryTable = sequelize.define('Category', {
+      id:{ type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      name: DataTypes.STRING,
+    },
+    {
+      tableName: 'categories',
+      underscored: true,
+      timestamps: false,
+    },
+  );
+
+  // CategoryTable.associate = (models) => {
+  //   CategoryTable.hasMany(models.PostCategory, {
+  //     as: 'post_category',
+  //     foreignKey: 'categoryId',
+  //   });
+  // };
+
+  return CategoryTable;
+};
+
+module.exports = Category;
