6336ebec192fc92b9a2e828336e58f6ce1396db0
Fabio Dalla Valle <fabiodvalle91@gmail.com>
2023-05-11 11:20:23 -0300

    requisito 8

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,8 @@ const login = require('../controllers/login');
 const userController = require('../controllers/userController');
 const validateJwt = require('../middlewares/validateJwt');
 const createUserValidate = require('../middlewares/createUserValidate');
+const categoryController = require('../controllers/categoryController');
+const createCategoryValidate = require('../middlewares/createCategoryValidate');
 
 const blogApiRoutes = express.Router();
 
@@ -10,5 +12,8 @@ blogApiRoutes.post('/login', login);
 blogApiRoutes.post('/user', createUserValidate, userController.createUser);
 blogApiRoutes.get('/user/:id', validateJwt, userController.getUserById);
 blogApiRoutes.get('/user', validateJwt, userController.getAllUsers);
+
+blogApiRoutes
+  .post('/categories', validateJwt, createCategoryValidate, categoryController.createCategory);
 
 module.exports = blogApiRoutes;
